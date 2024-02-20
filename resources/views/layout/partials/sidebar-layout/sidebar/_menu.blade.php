7075541f32e3042d9c155d68fe22fc077e816e10
JoaquinPolanco <joakinpolanco199726@gmail.com>
2024-02-20 11:17:58 -0600

    cambios menu y formulario crear empleados

--- resources/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
+++ resources/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
@@ -1,135 +1,66 @@
-<!--begin::sidebar menu-->
 <div class="app-sidebar-menu overflow-hidden flex-column-fluid">
-	<!--begin::Menu wrapper-->
 	<div id="kt_app_sidebar_menu_wrapper" class="app-sidebar-wrapper hover-scroll-overlay-y my-5" data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_app_sidebar_logo, #kt_app_sidebar_footer" data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px" data-kt-scroll-save-state="true">
-		<!--begin::Menu-->
 		<div class="menu menu-column menu-rounded menu-sub-indention px-3" id="#kt_app_sidebar_menu" data-kt-menu="true" data-kt-menu-expand="false">
-			<!--begin:Menu item-->
 			<div data-kt-menu-trigger="click" class="menu-item here show menu-accordion">
-				<!--begin:Menu link-->
 				<span class="menu-link">
 					<span class="menu-icon">{!! getIcon('element-11', 'fs-2') !!}</span>
 					<span class="menu-title">Dashboards</span>
 					<span class="menu-arrow"></span>
 				</span>
-				<!--end:Menu link-->
-				
-				
-				<!--end:Menu sub-->
 			</div>
-			<!--end:Menu item-->
-			<!--begin:Menu item-->
 			<div class="menu-item pt-5">
-				<!--begin:Menu content-->
 				<div class="menu-content">
 					<span class="menu-heading fw-bold text-uppercase fs-7">Pages</span>
 				</div>
-				<!--end:Menu content-->
 			</div>
-			<!--end:Menu item-->
-			<!--begin:Menu item-->
 			<div data-kt-menu-trigger="click" class="menu-item menu-accordion">
-				<!--begin:Menu link-->
 				<span class="menu-link">
 					<span class="menu-icon">{!! getIcon('address-book', 'fs-2') !!}</span>
 					<span class="menu-title">Empleados</span>
 					<span class="menu-arrow"></span>
 				</span>
-				<!--end:Menu link-->
-				<!--begin:Menu sub-->
 				<div class="menu-sub menu-sub-accordion">
-					<!--begin:Menu item-->
 					<div class="menu-item">
-						<!--begin:Menu link-->
 						<a class="menu-link" href="/empleado">
 							<span class="menu-bullet">
 								<span class="bullet bullet-dot"></span>
 							</span>
 							<span class="menu-title">Lista</span>
 						</a>
-						<!--end:Menu link-->
 					</div>
-					<!--end:Menu item-->
-					<!--begin:Menu item-->
-					
-
-
-
-
-					<!--end:Menu item-->
+					<div class="menu-item">
+						<a class="menu-link" href="/crear">
+							<span class="menu-bullet">
+								<span class="bullet bullet-dot"></span>
+							</span>
+							<span class="menu-title">Crear empleado</span>
+						</a>
+					</div>
 				</div>
-
-
-
-
-
 				<span class="menu-link">
 					<span class="menu-icon">{!! getIcon('address-book', 'fs-2') !!}</span>
 					<span class="menu-title">Gestión de envios</span>
 					<span class="menu-arrow"></span>
 				</span>
-				<!--end:Menu link-->
-				<!--begin:Menu sub-->
 				<div class="menu-sub menu-sub-accordion">
-					<!--begin:Menu item-->
 					<div class="menu-item">
-					<a class="menu-link" href="/envio/crear">
+						<a class="menu-link" href="/envio/crear">
 							<span class="menu-bullet">
 								<span class="bullet bullet-dot"></span>
 							</span>
 							<span class="menu-title">Crear envios</span>
 						</a>
-						<!--end:Menu link-->
 					</div>
-
-
 					<div class="menu-item">
-						<!--begin:Menu link-->
 						<a class="menu-link" href="/envio">
 							<span class="menu-bullet">
 								<span class="bullet bullet-dot"></span>
 							</span>
 							<span class="menu-title">Lista de envios</span>
 						</a>
-						<!--end:Menu link-->
 					</div>
-					<!--end:Menu item-->
-					<!--begin:Menu item-->
-						<!--begin:Menu link-->
-						
-
-
-
-
-					<!--end:Menu item-->
 				</div>
-
-
-
-
-
-				<!--end:Menu sub-->
 			</div>
-			<!--end:Menu item-->
-			<!--begin:Menu item-->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 		</div>
-		<!--end::Menu-->
 	</div>
-	<!--end::Menu wrapper-->
 </div>
-<!--end::sidebar menu-->
