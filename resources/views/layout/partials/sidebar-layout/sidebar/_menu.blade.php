0837501c0b69f7b6e9775a9e2ee52e5c079e3005
JoaquinPolanco <joakinpolanco199726@gmail.com>
2024-02-20 11:20:55 -0600

    cambio ruta empleados/crear al menu

--- resources/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
+++ resources/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
@@ -1,3 +1,4 @@
+
 <div class="app-sidebar-menu overflow-hidden flex-column-fluid">
 	<div id="kt_app_sidebar_menu_wrapper" class="app-sidebar-wrapper hover-scroll-overlay-y my-5" data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_app_sidebar_logo, #kt_app_sidebar_footer" data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px" data-kt-scroll-save-state="true">
 		<div class="menu menu-column menu-rounded menu-sub-indention px-3" id="#kt_app_sidebar_menu" data-kt-menu="true" data-kt-menu-expand="false">
@@ -29,7 +30,7 @@
 						</a>
 					</div>
 					<div class="menu-item">
-						<a class="menu-link" href="/crear">
+						<a class="menu-link" href="empleados/crear">
 							<span class="menu-bullet">
 								<span class="bullet bullet-dot"></span>
 							</span>
