13e1761a0a6c894114b4d308ead845b076236a23
JoaquinPolanco <joakinpolanco199726@gmail.com>
2024-02-21 20:13:00 -0600

    cambio vista crear

--- resources/views/empleados/crear.blade.php
+++ resources/views/empleados/crear.blade.php
@@ -35,7 +35,7 @@
                     </ul>
                 </div>
 
-                <div class="card mb-5 mb-xl-10 mx-auto" style="max-width: 950px;">
+                <div class="card mb-5 mb-xl-10">
                     <div class="card-header border-0 cursor-pointer" role="button" data-bs-toggle="collapse" data-bs-target="#kt_account_profile_details" aria-expanded="true" aria-controls="kt_account_profile_details">
                         <div class="card-title m-0">
                             <h3 class="fw-bold m-0">Crear Empleado</h3>
