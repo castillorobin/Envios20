d621b46cbea3612e6a1bec8e587b21ef04abf3d0
Robin Castillo <robin.castillo@catolica.edu.sv>
2024-03-10 00:03:22 -0600

    cambios en validacion

--- resources/views/envios/crearenvio.blade.php
+++ resources/views/envios/crearenvio.blade.php
@@ -7,6 +7,45 @@
             content: none;
         }
     </style>
+
+        
+<script>
+  $(document).ready(function() {
+  $("input").focusout(function() {
+    var value = $(this).val();
+    if (value.length == 0) {
+      $(this).addClass("is-invalid");
+      $(this).removeClass("is-valid");
+    } else {
+      $(this).removeClass("is-invalid");
+      $(this).addClass("is-valid");
+    }
+    /*
+           
+    */
+    console.log('Este campo es obligatorio');
+  });
+});
+
+$(document).ready(function() {
+  $("select").focusout(function() {
+    var value = $(this).val();
+    if (value.length == 0) {
+      $(this).addClass("is-invalid");
+      $(this).removeClass("is-valid");
+    } else {
+      $(this).removeClass("is-invalid");
+      $(this).addClass("is-valid");
+    }
+    /*
+           
+    */
+    console.log('Este campo es obligatorio');
+  });
+});
+</script>
+
+
     <div class="app-content flex-column-fluid">
         <div class="app-container container-xxl">
             <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3 pt-4 mb-4">
@@ -59,11 +98,11 @@
 
                                 <div class="row my-4 mx-4">
                                     <div class="form-floating col-lg-3 mb-4">
-                                        <input type="text" class="form-control form-control-solid" name="n_guia" id="n_guia" placeholder="# de guia" />
+                                        <input type="text" class="form-control form-control-solid" name="n_guia" id="n_guia" placeholder="# de guia" required/>
                                         <label for="n_guia" style="padding-left: 25px;"># de guia</label>
-                                        <div id="n_guiaValidationFeedback" class="invalid-feedback">
-                                            Por favor ingrese solo números para la guía.
-                                        </div>
+                                        
+                                        <div class="invalid-feedback">Este campo es obligatorio.</div>
+        <div class="valid-feedback"><i class="fas fa-check-circle"></i>&nbsp;Correcto</div>   
                                     </div>
                                 </div>
                                 <div class="row my-4 mx-4">
@@ -219,286 +258,17 @@
         </div>
     </div>
 </x-default-layout>
+
+
 <link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
 <script src="assets/plugins/global/plugins.bundle.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
     flatpickr("#fecha_entregap", {
         dateFormat: 'Y-m-d',
     });
 </script>
-<script>
-    // Función para validar el campo de comercio
-    function validarComercio() {
-        var selectValue = document.getElementById('comercio').value;
-        var validationFeedback = document.getElementById('comercioValidationFeedback');
-        if (selectValue === 'selecione el comercio') {
-            document.getElementById('comercio').classList.remove('is-valid');
-            document.getElementById('comercio').classList.add('is-invalid');
-            validationFeedback.style.display = 'block';
-            return false;
-        } else {
-            document.getElementById('comercio').classList.remove('is-invalid');
-            document.getElementById('comercio').classList.add('is-valid');
-            validationFeedback.style.display = 'none';
-            return true;
-        }
-    }
 
-    // Función para validar el campo de guía
-    function validarGuia() {
-        var n_guiaValue = document.getElementById('n_guia').value;
-        var n_guiaValidationFeedback = document.getElementById('n_guiaValidationFeedback');
-        if (!n_guiaValue.trim()) {
-            document.getElementById('n_guia').classList.remove('is-valid');
-            document.getElementById('n_guia').classList.add('is-invalid');
-            n_guiaValidationFeedback.textContent = 'Por favor no dejar en blanco, ingrese un número de guía.';
-            n_guiaValidationFeedback.style.display = 'block';
-            return false;
-        } else if (!/^[0-9]+$/.test(n_guiaValue)) {
-            document.getElementById('n_guia').classList.remove('is-valid');
-            document.getElementById('n_guia').classList.add('is-invalid');
-            n_guiaValidationFeedback.textContent = 'Por favor ingrese solo números para la guía.';
-            n_guiaValidationFeedback.style.display = 'block';
-            return false;
-        } else {
-            document.getElementById('n_guia').classList.remove('is-invalid');
-            document.getElementById('n_guia').classList.add('is-valid');
-            n_guiaValidationFeedback.style.display = 'none';
-            return true;
-        }
-    }
-
-    // Función para validar el campo de destinatario
-    function validarDestinatario() {
-        var destinatarioValue = document.getElementById('destinatariop').value;
-        var destinatarioValidationFeedback = document.getElementById('destinatarioValidationFeedback');
-        if (!destinatarioValue.trim()) {
-            document.getElementById('destinatariop').classList.remove('is-valid');
-            document.getElementById('destinatariop').classList.add('is-invalid');
-            destinatarioValidationFeedback.style.display = 'block';
-            return false;
-        } else {
-            document.getElementById('destinatariop').classList.remove('is-invalid');
-            document.getElementById('destinatariop').classList.add('is-valid');
-            destinatarioValidationFeedback.style.display = 'none';
-            return true;
-        }
-    }
-    // Función para validar el campo de dirrecion
-    function validarDireccion() {
-        var direccionValue = document.getElementById('direccionp').value;
-        var direccionValidationFeedback = document.getElementById('direccionValidationFeedback');
-        if (!direccionValue.trim()) {
-            document.getElementById('direccionp').classList.remove('is-valid');
-            document.getElementById('direccionp').classList.add('is-invalid');
-            direccionValidationFeedback.style.display = 'block';
-            return false;
-        } else {
-            document.getElementById('direccionp').classList.remove('is-invalid');
-            document.getElementById('direccionp').classList.add('is-valid');
-            direccionValidationFeedback.style.display = 'none';
-            return true;
-        }
-    }
-    // Función para validar el campo de Telefono
-    function validarTelefono() {
-        var telefonoValue = document.getElementById('telefonop').value;
-        var telefonoValidationFeedback = document.getElementById('telefonoValidationFeedback');
-        var telefonoPattern = /^\d+$/;
-
-        if (!telefonoValue.trim() || !telefonoPattern.test(telefonoValue)) {
-            document.getElementById('telefonop').classList.remove('is-valid');
-            document.getElementById('telefonop').classList.add('is-invalid');
-            telefonoValidationFeedback.style.display = 'block';
-            return false;
-        } else {
-            document.getElementById('telefonop').classList.remove('is-invalid');
-            document.getElementById('telefonop').classList.add('is-valid');
-            telefonoValidationFeedback.style.display = 'none';
-            return true;
-        }
-    }
-    // Función para validar los cmpos de precio y total
-    function validarPrecioPaquete() {
-        return validarNumeroNoVacio('precio', 'precioValidationFeedback', 'Por favor ingrese un precio válido para el paquete.');
-    }
-
-    function validarPrecioEnvio() {
-        return validarNumeroNoVacio('envio', 'envioValidationFeedback', 'Por favor ingrese un precio válido para el envío.');
-    }
-
-    function validarTotalPagar() {
-        return validarNumeroNoVacio('total', 'totalValidationFeedback', 'Por favor ingrese un total válido a pagar.');
-    }
-
-    function validarNumeroNoVacio(inputId, feedbackId, mensajeError) {
-        var inputValue = document.getElementById(inputId).value;
-        var validationFeedback = document.getElementById(feedbackId);
-        var numberPattern = /^\d+(\.\d{1,2})?$/;
-
-        if (!inputValue.trim() || !numberPattern.test(inputValue)) {
-            document.getElementById(inputId).classList.remove('is-valid');
-            document.getElementById(inputId).classList.add('is-invalid');
-            validationFeedback.style.display = 'block';
-            return false;
-        } else {
-            document.getElementById(inputId).classList.remove('is-invalid');
-            document.getElementById(inputId).classList.add('is-valid');
-            validationFeedback.style.display = 'none';
-            return true;
-        }
-    }
-    // Función para validar el campo de cobro de envio
-    function validarCobroEnvio() {
-        var selectValue = document.getElementById('cenvio').value;
-        var validationFeedback = document.getElementById('cenviovalidationFeedback');
-        if (selectValue === 'seleccione el estado cobro') {
-            document.getElementById('cenvio').classList.remove('is-valid');
-            document.getElementById('cenvio').classList.add('is-invalid');
-            validationFeedback.style.display = 'block';
-            return false;
-        } else {
-            document.getElementById('cenvio').classList.remove('is-invalid');
-            document.getElementById('cenvio').classList.add('is-valid');
-            validationFeedback.style.display = 'none';
-            return true;
-        }
-    }
-
-    // Función para validar el campo de estado del pago
-    function validarEstadoPago() {
-        var selectValue = document.getElementById('estado_pago').value;
-        var validationFeedback = document.getElementById('estadoPagoValidationFeedback');
-        if (selectValue === 'seleccione el estado del pago') {
-            document.getElementById('estado_pago').classList.remove('is-valid');
-            document.getElementById('estado_pago').classList.add('is-invalid');
-            validationFeedback.style.display = 'block';
-            return false;
-        } else {
-            document.getElementById('estado_pago').classList.remove('is-invalid');
-            document.getElementById('estado_pago').classList.add('is-valid');
-            validationFeedback.style.display = 'none';
-            return true;
-        }
-    }
-
-    // Función para validar el campo de tipo de envio
-    function validarTipoEnvio() {
-        var selectValue = document.getElementById('tipo_envio').value;
-        var validationFeedback = document.getElementById('tipoEnvioValidationFeedback');
-        if (selectValue === 'Seleccione el tipo de envío') {
-            document.getElementById('tipo_envio').classList.remove('is-valid');
-            document.getElementById('tipo_envio').classList.add('is-invalid');
-            validationFeedback.style.display = 'block';
-            return false;
-        } else {
-            document.getElementById('tipo_envio').classList.remove('is-invalid');
-            document.getElementById('tipo_envio').classList.add('is-valid');
-            validationFeedback.style.display = 'none';
-            return true;
-        }
-    }
-    // Función para validar el campo de estado del envio
-    function validarEstadoEnvio() {
-        var selectValue = document.getElementById('estado_enviop').value;
-        var validationFeedback = document.getElementById('estadoEnviopValidationFeedback');
-        if (selectValue === 'Seleccione el estado del envío') {
-            document.getElementById('estado_enviop').classList.remove('is-valid');
-            document.getElementById('estado_enviop').classList.add('is-invalid');
-            validationFeedback.style.display = 'block';
-            return false;
-        } else {
-            document.getElementById('estado_enviop').classList.remove('is-invalid');
-            document.getElementById('estado_enviop').classList.add('is-valid');
-            validationFeedback.style.display = 'none';
-            return true;
-        }
-    }
-    // Función para validar el campo de fecha
-    function validarFechaEntrega() {
-        var inputElement = document.getElementById('fecha_entregap');
-        var validationFeedback = document.getElementById('fechaEntregapValidationFeedback');
-
-        if (inputElement.value === '') {
-            inputElement.classList.remove('is-valid');
-            inputElement.classList.add('is-invalid');
-            validationFeedback.style.display = 'block';
-            return false; // Valor inválido
-        } else {
-            inputElement.classList.remove('is-invalid');
-            inputElement.classList.add('is-valid');
-            validationFeedback.style.display = 'none';
-            return true; // Valor válido
-        }
-    }
-    // Evento para validar el campo de comercio al cambiar
-    document.getElementById('comercio').addEventListener('change', validarComercio);
-    // Evento para validar el campo de guía al escribir
-    document.getElementById('n_guia').addEventListener('input', validarGuia);
-    // Evento para validar el campo destinatario
-    document.getElementById('destinatariop').addEventListener('input', validarDestinatario);
-    // Evento para validar el campo de dirección al escribir
-    document.getElementById('direccionp').addEventListener('input', validarDireccion);
-    // Evento para validar el campo de teléfono al escribir
-    document.getElementById('telefonop').addEventListener('input', validarTelefono);
-    // Eventos para validar los campos al precio y total
-    document.getElementById('precio').addEventListener('input', validarPrecioPaquete);
-    document.getElementById('envio').addEventListener('input', validarPrecioEnvio);
-    document.getElementById('total').addEventListener('input', validarTotalPagar);
-    // Evento para validar el campo al cambiar la selección
-    document.getElementById('cenvio').addEventListener('change', validarCobroEnvio);
-    // Evento para validar el campo de estado de pago al perder el foco
-    document.getElementById('estado_pago').addEventListener('change', validarEstadoPago);
-
-    // Evento para validar el campo al cambiar la selección
-    document.getElementById('tipo_envio').addEventListener('change', validarTipoEnvio);
-    // Evento para validar el campo al cambiar la selección
-    document.getElementById('estado_enviop').addEventListener('change', validarEstadoEnvio);
-    // Evento para validar el campo al cambiar la fecha
-    document.getElementById('fecha_entregap').addEventListener('change', validarFechaEntrega);
-
-
-    // Evento para validar el formulario antes de enviar
-    document.getElementById('kt_ecommerce_settings_general_form').addEventListener('submit', function(event) {
-        var comercioValido = validarComercio();
-        var guiaValida = validarGuia();
-        var destinatarioValido = validarDestinatario();
-        var direccionValida = validarDireccion();
-        var telefonoValido = validarTelefono();
-        var precioPaqueteValido = validarPrecioPaquete();
-        var precioEnvioValido = validarPrecioEnvio();
-        var totalPagarValido = validarTotalPagar();
-        var cobroEnvioValido = validarCobroEnvio();
-        var estadoPagoValido = validarEstadoPago();
-        var tipoEnvioValido = validarTipoEnvio();
-        var estadoEnvioValido = validarEstadoEnvio();
-        var fechaEntregaValida = validarFechaEntrega();
-
-        if (!comercioValido || !guiaValida || !destinatarioValido || !direccionValida || !telefonoValido || !precioPaqueteValido || !precioEnvioValido || !totalPagarValido || !cobroEnvioValido || !estadoPagoValido || !tipoEnvioValido || !estadoEnvioValido || !fechaEntregaValida) {
-            event.preventDefault(); // Detener el envío del formulario
-
-            // Mostrar alerta indicando los campos que faltan
-            alert("Por favor complete todos los campos antes de guardar.");
-        }
-
-        // Validar campos vacíos
-        var camposVacios = [];
-        var inputs = document.querySelectorAll('input, select, textarea');
-        inputs.forEach(function(input) {
-            if (!input.value.trim()) {
-                camposVacios.push(input);
-            }
-        });
-
-        if (camposVacios.length > 0) {
-            event.preventDefault();
-            camposVacios[0].focus();
-            camposVacios.forEach(function(campo) {
-                campo.classList.add('is-invalid');
-            });
-        }
-    });
-</script>
 <script>
     $(document).ready(function() {
 
