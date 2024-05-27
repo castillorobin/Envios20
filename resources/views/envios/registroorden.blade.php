a47982fadf90243198a2b9153bc00545830e8028
castillorobin <robin.castillo@catolica.edu.sv>
2024-05-27 08:44:02 -0600

    cambios en cobros

--- resources/views/envios/registroorden.blade.php
+++ resources/views/envios/registroorden.blade.php
@@ -30,6 +30,46 @@
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 
 
+<script>  
+/*
+    jQuery(document).ready(function($){
+        $(document).ready(function() {
+            $('.mi-selector').select2();
+           
+            $('#comercio').on('select2:select', function (e) { 
+                
+              alert("Hola mundo");
+            });
+    
+        });
+       
+       
+    });
+    */
+    
+    
+    </script>
+
+<script>
+    function valor() {
+
+       // alert("hola");
+       
+        nombre =  document.getElementById("comercio").value;
+        nom=document.getElementById("dir" + nombre).value;
+        document.getElementById("direccion").value = nom;
+
+        corr=document.getElementById("cor" + nombre).value;
+        document.getElementById("correo").value = corr;
+
+        tele=document.getElementById("tel" + nombre).value;
+        document.getElementById("telefono").value = tele;
+        
+       
+    }
+    
+    </script>
+
 <body id="kt_body" class="" style="background-color:white; ">
     <x-default-layout>
        
@@ -62,7 +102,7 @@
 <div  class="page-title d-flex flex-column justify-content-center flex-wrap me-3 ">
     <!--begin::Title-->
     <h1 class="page-heading d-flex text-gray-900 fw-bold fs-3 flex-column justify-content-center my-0">
-        Create
+        Crear
             </h1>
     <!--end::Title-->
 
@@ -72,7 +112,7 @@
                             <!--begin::Item-->
                                     <li class="breadcrumb-item text-muted">
                                                     <a href="/metronic8/demo1/index.html" class="text-muted text-hover-primary">
-                                Home                            </a>
+                                Inicio                            </a>
                                             </li>
                                 <!--end::Item-->
                                     <!--begin::Item-->
@@ -83,7 +123,7 @@
                                         
                             <!--begin::Item-->
                                     <li class="breadcrumb-item text-muted">
-                                                    Invoice Manager                                            </li>
+                                                    Cobros                                           </li>
                                 <!--end::Item-->
                                         
                     </ul>
@@ -93,109 +133,7 @@
 <!--begin::Actions-->
 <div class="d-flex align-items-center gap-2 gap-lg-3">
             <!--begin::Filter menu-->
-        <div class="m-0">
-            <!--begin::Menu toggle-->
-            <a href="#" class="btn btn-sm btn-flex btn-secondary fw-bold" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-                <i class="ki-duotone ki-filter fs-6 text-muted me-1"><span class="path1"></span><span class="path2"></span></i>               
-                Filter
-            </a>
-            <!--end::Menu toggle-->
-            
-            
-
-<!--begin::Menu 1-->
-<div class="menu menu-sub menu-sub-dropdown w-250px w-md-300px" data-kt-menu="true" id="kt_menu_66510cbc53d19">
-    <!--begin::Header-->
-    <div class="px-7 py-5">
-        <div class="fs-5 text-gray-900 fw-bold">Filter Options</div>
-    </div>
-    <!--end::Header-->
-
-    <!--begin::Menu separator-->
-    <div class="separator border-gray-200"></div>
-    <!--end::Menu separator-->
-    
-
-    <!--begin::Form-->
-    <div class="px-7 py-5">
-        <!--begin::Input group-->
-        <div class="mb-10">
-            <!--begin::Label-->
-            <label class="form-label fw-semibold">Status:</label>
-            <!--end::Label-->
-
-            <!--begin::Input-->
-            <div>
-                <select class="form-select form-select-solid" multiple data-kt-select2="true" data-close-on-select="false" data-placeholder="Select option" data-dropdown-parent="#kt_menu_66510cbc53d19" data-allow-clear="true">
-                    <option></option>
-                    <option value="1">Approved</option>
-                    <option value="2">Pending</option>
-                    <option value="2">In Process</option>
-                    <option value="2">Rejected</option>
-                </select>
-            </div>
-            <!--end::Input-->
-        </div>
-        <!--end::Input group-->
-
-        <!--begin::Input group-->
-        <div class="mb-10">
-            <!--begin::Label-->
-            <label class="form-label fw-semibold">Member Type:</label>
-            <!--end::Label-->
-
-            <!--begin::Options-->
-            <div class="d-flex">
-                <!--begin::Options-->    
-                <label class="form-check form-check-sm form-check-custom form-check-solid me-5">
-                    <input class="form-check-input" type="checkbox" value="1"/>
-                    <span class="form-check-label">
-                        Author
-                    </span>
-                </label>
-                <!--end::Options-->    
-
-                <!--begin::Options-->    
-                <label class="form-check form-check-sm form-check-custom form-check-solid">
-                    <input class="form-check-input" type="checkbox" value="2" checked="checked"/>
-                    <span class="form-check-label">
-                        Customer
-                    </span>
-                </label>
-                <!--end::Options-->    
-            </div>        
-            <!--end::Options-->    
-        </div>
-        <!--end::Input group-->
-
-        <!--begin::Input group-->
-        <div class="mb-10">
-            <!--begin::Label-->
-            <label class="form-label fw-semibold">Notifications:</label>
-            <!--end::Label-->
-
-            <!--begin::Switch-->
-            <div class="form-check form-switch form-switch-sm form-check-custom form-check-solid">
-                <input class="form-check-input" type="checkbox" value="" name="notifications" checked />
-                <label class="form-check-label">
-                    Enabled
-                </label>
-            </div>
-            <!--end::Switch-->
-        </div>
-        <!--end::Input group-->
-
-        <!--begin::Actions-->
-        <div class="d-flex justify-content-end">
-            <button type="reset" class="btn btn-sm btn-light btn-active-light-primary me-2" data-kt-menu-dismiss="true">Reset</button>
-
-            <button type="submit" class="btn btn-sm btn-primary" data-kt-menu-dismiss="true">Apply</button>
-        </div>
-        <!--end::Actions-->
-    </div>
-    <!--end::Form-->
-</div>
-<!--end::Menu 1-->        </div>
+       
         <!--end::Filter menu-->
     
     
@@ -204,7 +142,7 @@
     
     <!--begin::Primary button-->
             <a href="#" class="btn btn-sm fw-bold btn-primary"  data-bs-toggle="modal" data-bs-target="#kt_modal_create_app">
-            Create        </a>
+            Crear     </a>
         <!--end::Primary button-->
 </div>
 <!--end::Actions-->
@@ -281,18 +219,26 @@
 
                         <!--begin::Input group-->
                         <div class="mb-5">
-                            <select class="form-control form-control-solid " data-control="select2" name="comercio" id="comercio">
+                            <select class="form-select form-select-solid mi-selector" data-control="select2" name="comercio" id="comercio" onchange="valor()">
                                 <option value=" ">Buscar comercio</option>
                                 @foreach ($comercios as $comercio)
                                 <option value="{{$comercio->comercio}}">{{$comercio->comercio}}</option>
                                 @endforeach
                             </select>
                         </div>
+
+                        @foreach ($comercios as $comercio)
+                        <input hidden id="dir{{$comercio->comercio}}" value="{{ $comercio->direccion }} ">
+                        <input hidden id="cor{{$comercio->comercio}}" value="{{ $comercio->correo }} ">
+                        <input hidden id="tel{{$comercio->comercio}}" value="{{ $comercio->telefono }} ">
+                            
+                                @endforeach
+
                         <!--end::Input group-->
 
                         <!--begin::Input group-->
                         <div class="mb-5">
-                            <input type="text" class="form-control form-control-solid" placeholder="Email"/>
+                            <input type="text" class="form-control form-control-solid" placeholder="Correo" name="correo" id="correo" />
                         </div>
                         <!--end::Input group-->
 
@@ -306,17 +252,17 @@
 
                     <!--begin::Col-->
                     <div class="col-lg-6">
-                        <label class="form-label fs-6 fw-bold text-gray-700 mb-3">Bill To</label>
+                        <label class="form-label fs-6 fw-bold text-gray-700 mb-3"> &nbsp;</label>
 
                         <!--begin::Input group-->
                         <div class="mb-5">
-                            <input type="text" class="form-control form-control-solid" placeholder="Name"/>
+                            <input type="text" class="form-control form-control-solid" placeholder="Direccion" name="direccion" id="direccion" />
                         </div>
                         <!--end::Input group-->
 
                         <!--begin::Input group-->
                         <div class="mb-5">
-                            <input type="text" class="form-control form-control-solid" placeholder="Email"/>
+                            <input type="text" class="form-control form-control-solid" placeholder="Telefono" name="telefono" id="telefono" />
                         </div>
                         <!--end::Input group-->
 
@@ -489,7 +435,7 @@
             <!--end::Label-->
 
             <!--begin::Select-->           
-            <select name="currnecy" aria-label="Select a Timezone" data-control="select2" data-placeholder="Select currency" class="form-select form-select-solid">
+            <select name="currnecy" aria-label="Select a Timezone" data-control="select2"  class="form-select form-select-solid">
                 <option value=""></option>
 
                                     <option data-kt-flag="flags/united-states.svg" value="USD"><b>USD</b>&nbsp;-&nbsp;USA dollar</option>
