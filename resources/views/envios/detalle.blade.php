7ceeed63879afd0dc65d0fdb6a1e9960319ef009
Robin Castillo <robin.castillo@catolica.edu.sv>
2024-09-03 15:49:14 -0600

    cambios en barra de iconos

--- resources/views/envios/detalle.blade.php
+++ resources/views/envios/detalle.blade.php
@@ -216,12 +216,11 @@
                                     
                                     <h2 class="fw-bold m-0" style="color:steelblue">{{ $envio[0]->guia }}</h2>
                                     
-                                    
                                 </div>
-                                <div class="alert alert-primary d-flex align-items-center" role="alert">
-                                    <i class="fas fa-store-alt" style="color:steelblue"></i>
-                                    <div style="font-size: 18px">
-                                    &nbsp; &nbsp;  {{ $envio[0]->estado }}                                    </div>
+                                <div class="alert alert-primary d-flex align-items-center " role="alert" style="border-radius: 10px 40px 40px 10px; padding:5px; margin: 2px;">
+                                    <i class="fas fa-store-alt" style="color:steelblue;"></i>
+                                    <div style="font-size: 16px; margin: 2px;">
+                                    &nbsp; &nbsp;  {{ $envio[0]->estado }} </div>
                                   </div>
                                 </div>
 
@@ -230,22 +229,22 @@
 
                             @if( $envio[0]->estado  == "Creado")
                                 <div class="row p-3 text-center">
-                                    <div class="col-2 text-center">
-                                        <i class="fas fa-store-alt" style="font-size:45px; color:steelblue"></i> 
+                                    <div class="col-1 text-center">
+                                        <i class="fas fa-store-alt" style="font-size:35px; color:steelblue"></i> 
                                         <h4 style="margin-top: 0.25em;">Creado</h4>
                                     </div>
-                                    <div class="col-3">
-                                        <hr style="background-color: green; height: 15px; border: 0; opacity: 1; ">
+                                    <div class="col-4">
+                                        <hr style="background-color: gray; height: 8px; border: 0; opacity: 1; border-radius: 20px;">
                                     </div>
-                                    <div class="col-2 ">
-                                        <i class="fas fa-shipping-fast" style="font-size:45px; color:steelblue"></i>  
+                                    <div class="col-1 ">
+                                        <i class="fas fa-shipping-fast" style="font-size:35px; color:steelblue"></i>  
                                         <h4 style="margin-top: 0.25em;">En Ruta</h4>
                                     </div>
-                                    <div class="col-3">
-                                        <hr style="background-color: gray; height: 15px; border: 0; opacity: 1; ">
+                                    <div class="col-4">
+                                        <hr style="background-color: gray; height: 8px; border: 0; opacity: 1; border-radius: 20px;">
                                     </div>
-                                    <div class="col-2 ">
-                                    <i class="fas fa-calendar-times" style="font-size:45px; color:steelblue"></i> 
+                                    <div class="col-1 ">
+                                    <i class="fas fa-calendar-times" style="font-size:35px; color:steelblue"></i> 
                                         <h4 style="margin-top: 0.25em;">Entregado</h4>
                                     </div>
                                 </div>
