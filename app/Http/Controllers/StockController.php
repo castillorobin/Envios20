281166af6154ce86810836a71de6e53f911227c4
Robin Castillo <robin.castillo@catolica.edu.sv>
2024-11-23 08:06:08 -0600

    cambios en cajas y stocks

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -156,6 +156,8 @@ class StockController extends Controller
             $envio->nivel = $nivel;
             $envio->caja = $caja;
             $envio->tarima = $tarima;
+            $envio->agencia = $agencia;
+            $envio->ubicacion = $cajasuelto;
             } 
 
             if($cajasuelto=="suelto"){
@@ -163,6 +165,8 @@ class StockController extends Controller
             $envio->nivel = $nivels;
             //$envio->caja = $caja;
             $envio->tarima = $tarimas;
+            $envio->agencia = $agencia;
+            $envio->ubicacion = $cajasuelto;
             } 
             
 
--- resources/views/stocks/asignarrepartidorcajadatos.blade.php
+++ resources/views/stocks/asignarrepartidorcajadatos.blade.php
@@ -67,7 +67,7 @@ if(tipo=='suelto'){
 <body id="kt_app_body" data-kt-app-layout="dark-sidebar" data-kt-app-header-fixed="true" data-kt-app-sidebar-enabled="true" data-kt-app-sidebar-fixed="true" data-kt-app-sidebar-hoverable="true" data-kt-app-sidebar-push-header="true" data-kt-app-sidebar-push-toolbar="true" data-kt-app-sidebar-push-footer="true" data-kt-app-toolbar-enabled="true" class="app-default">
 
     <x-default-layout>
-        <div class="app-main flex-column flex-row-fluid" id="kt_app_main">
+        <div class="app-main flex-column flex-row-fluid" id="kt_app_main" style="margin-top: -355px;">
             <!--begin::Content wrapper-->
             <div class="d-flex flex-column flex-column-fluid">
                 <!--begin::Toolbar-->
