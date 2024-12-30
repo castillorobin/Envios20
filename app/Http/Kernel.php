a1b6db8e64552ba1e8731afb51a29d42e3b608e7
Robin Castillo <robin.castillo@catolica.edu.sv>
2024-12-29 23:42:48 -0600

    cambios en usuarios activos

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,7 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        // \App\Http\Middleware\UserActivityNow::class,
+            \App\Http\Middleware\UserActivityNow::class,
         ],
 
         'api' => [
@@ -69,6 +69,6 @@ class Kernel extends HttpKernel
         'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
-      // 'UserActivityNow' => \App\Http\Middleware\UserActivityNow::class,
+       'UserActivityNow' => \App\Http\Middleware\UserActivityNow::class,
     ];
 }
