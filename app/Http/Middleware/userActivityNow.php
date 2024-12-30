853bf6ebed05f9cec0f4ad16886338afe1631317
Robin Castillo <robin.castillo@catolica.edu.sv>
2024-12-29 23:53:42 -0600

    cambios en actividad

--- app/Http/Middleware/userActivityNow.php
+++ app/Http/Middleware/userActivityNow.php
@@ -9,7 +9,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class userActivityNow
+class userActivityNow extends Middleware
 {
     /**
      * Handle an incoming request.
