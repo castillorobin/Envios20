5fb5d513977b4f9b666b8ce2ffe53ef2b4dc2cc8
Robin Castillo <robin.castillo@catolica.edu.sv>
2025-04-09 11:59:57 -0600

    cambios en reporte cobro

--- resources/views/envios/listadoticketdatos.blade.php
+++ resources/views/envios/listadoticketdatos.blade.php
@@ -192,7 +192,7 @@ if (searchText == "") {
                         <!--begin::Page title-->
                         <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
                             <!--begin::Title-->
-                            <h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">Reporte de pago</h1>
+                            <h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">Reporte de cobro</h1>
                             <!--end::Title-->
                             <!--begin::Breadcrumb-->
                             <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
@@ -207,7 +207,7 @@ if (searchText == "") {
                                 </li>
                                 <!--end::Item-->
                                 <!--begin::Item-->
-                                <li class="breadcrumb-item text-muted">Reporte de pago</li>
+                                <li class="breadcrumb-item text-muted">Reporte de cobro</li>
                                 <!--end::Item-->
                             </ul>
                             <!--end::Breadcrumb-->
