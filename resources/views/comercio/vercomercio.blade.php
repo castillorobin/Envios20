465715c3dde0bd8b93ef087e61f7327dd5ad3b94
Robin Castillo <robin.castillo@catolica.edu.sv>
2025-06-24 09:27:30 -0600

    cambios editando

--- resources/views/comercio/vercomercio.blade.php
+++ resources/views/comercio/vercomercio.blade.php
@@ -262,7 +262,7 @@
                                 <div class="row mb-6">
                                     <label class="col-lg-3 col-form-label">Tipo de contribuyente</label>
                                     <div class="col-lg-3">
-                                       <span>{{ $comercio->tipo_cuenta }}</span>
+                                       <span>{{ $comercio->tipo_contri }}</span>
                                     </div>
                                     <label class="col-lg-2 col-form-label">Giro</label>
                                     <div class="col-lg-3">
@@ -304,10 +304,209 @@
 <!--end::Additional details-->            </div>
             <!--end:::Tab pane-->
 
-            <!--begin:::Tab pane-->
+            
             <div class="tab-pane fade" id="kt_contact_view_meetings" role="tabpanel">
-      Editar
-<!--end::Tab Content-->            </div>
+      
+<!--begin:::Editar-->
+ <form action="/comercio/editando" method="POST" id="kt_account_profile_details_form" class="form">
+                            @csrf
+                            @method('GET')
+                            <div class="card-body border-top p-9">
+                                <div class="row mb-6">
+                                    <label class="col-lg-3 col-form-label required fw-semibold fs-6">Nombre del titular</label>
+                                    <div class="col-lg-8">
+                                        <input type="text" name="ftitu" id="ftitu" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" value="{{ $comercio->titular }} " />
+                                        <div class="invalid-feedback">Este campo es obligatorio.</div>
+                                    </div>
+                                </div>
+                                <div class="row mb-6">
+                                    <label class="col-lg-3 col-form-label required fw-semibold fs-6">Nombre del comercio</label>
+                                    <div class="col-lg-8">
+                                        <input type="text" name="fname" id="fname" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" value="{{ $comercio->comercio }} " />
+                                        <div class="invalid-feedback">Este campo es obligatorio.</div>
+                                    </div>
+                                </div>
+
+                                <div class="row mb-6">
+                                    <label class="col-lg-3 col-form-label">Dirección del comercio</label>
+                                    <div class="col-lg-8">
+                                        <input type="text" name="direccion" class="form-control form-control-lg form-control-solid" value=" {{ $comercio->direccion }}" />
+                                    </div>
+                                </div>
+                                <div class="row mb-6">
+                                    <label class="col-lg-3 col-form-label">Telefono</label>
+                                    <div class="col-lg-3">
+                                        <input type="tel" name="telefono" class="form-control form-control-lg form-control-solid" value=" {{ $comercio->telefono }}" />
+                                    </div>
+
+                                    <label class="col-lg-2 col-form-label">WhatsApp</label>
+                                    <div class="col-lg-3">
+                                        <input type="tel" name="whatsapp" class="form-control form-control-lg form-control-solid" value="{{ $comercio->whatsapp }} " />
+                                    </div>
+                                </div>
+                                <div class="row mb-6">
+                                    <label class="col-lg-3 col-form-label required fw-semibold fs-6">Fecha de Alta</label>
+                                    <div class="col-lg-3">
+                                        <input type="text" name="fecha_alta" class="form-control form-control-lg form-control-solid" id="fecha_alta" value="{{ $comercio->f_alta }} " />
+                                    </div>
+
+                                    <label class="col-lg-2 col-form-label">Fecha de baja</label>
+                                    <div class="col-lg-3">
+                                        <input type="text" name="fecha_baja" class="form-control form-control-lg form-control-solid" id="fecha_baja" value="{{ $comercio->f_baja }} " />
+                                    </div>
+                                </div>
+                                <div class="row mb-6">
+                                    <label class="col-lg-3 col-form-label">Tipo de comercio</label>
+                                    <div class="col-lg-3">
+                                        <select name="tipo" id="Tipo" class="form-control form-control-lg form-control-solid" data-control="select2" data-placeholder="Seleccionar Tipo de comercio" required>
+                                          <option value="{{ $comercio->tipo }}">{{ $comercio->tipo }}</option>  
+                                        <option value="Emprendedor">Emprendedor</option>
+                                            <option value="Comercio Mediano">Comercio Mediano</option>
+                                            <option value="Empresa">Empresa</option>
+                                        </select>
+                                    </div>
+                                    <label class="col-lg-2 col-form-label required fw-semibold fs-6">Estado de comercio</label>
+                                    <div class="col-lg-3">
+                                        <select name="Estado_comercio" id="Estado_comercio" class="form-control form-control-lg form-control-solid" data-control="select2" data-placeholder="Seleccionar Estado de comercioo" required>
+                                        <option value="{{ $comercio->estado }}">{{ $comercio->estado }}   </option> 
+                                        <option value="Alta">Alta</option>{{ $comercio->estado }}
+                                            <option value="Baja">Baja</option>
+                                            <option value="Lista negra">Lista negra</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="row mb-6">
+                                    <label class="col-lg-3 col-form-label required fw-semibold fs-6">Agencia de registro</label>
+                                    <div class="col-lg-8">
+                                        <select name="agencia_registro" id="Agencia_registro" class="form-control form-control-lg form-control-solid" data-control="select2" data-placeholder="Seleccionar Agencia de registro" required>
+                                        <option value="{{ $comercio->agencia }}">{{ $comercio->agencia }}</option>    
+                                        <option value="San Salvador">San Salvador</option>
+                                            <option value="San Miguel">San Miguel</option>
+                                            <option value="Santa Ana">Santa Ana</option>
+                                            <option value="Centro de distribucion">Centro de distribucion</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="row mb-6">
+                                    <label class="col-lg-3 col-form-label">Correo Electrónico</label>
+                                    <div class="col-lg-8">
+                                        <input type="email" name="email" class="form-control form-control-lg form-control-solid" value="{{ $comercio->correo }} " />
+                                    </div>
+                                </div>
+                                <div class="row mb-6">
+                                    <label class="col-lg-3 col-form-label">Nota</label>
+                                    <div class="col-lg-8">
+                                        <input type="text" name="nota" class="form-control form-control-lg form-control-solid" value="{{ $comercio->nota }} " />
+                                    </div>
+                                </div>
+                                <div class="separator my-10"></div>
+                                <h4 class="text-center">
+                                    Datos Bancarios
+                                </h4>
+                                <br>
+                                <div class="row mb-6">
+                                    <label class="col-lg-3 col-form-label">Nombre del titular de la cuenta</label>
+                                    <div class="col-lg-8">
+                                        <input type="text" name="titular" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" value="{{ $comercio->titular }} " />
+                                    </div>
+                                </div>
+                                <div class="row mb-6">
+                                    <label class="col-lg-3 col-form-label">Nombre del banco</label>
+                                    <div class="col-lg-3">
+                                        <input type="text" name="banco" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" value="{{ $comercio->banco }} " />
+                                    </div>
+                                    <label class="col-lg-2 col-form-label">Numero de cuenta</label>
+                                    <div class="col-lg-3">
+                                        <input type="text" name="cuenta" class="form-control form-control-lg form-control-solid" value="{{ $comercio->cuenta }} " />
+                                    </div>
+                                </div>
+                                <div class="row mb-6">
+                                    <label class="col-lg-3 col-form-label">Numero de DUI</label>
+                                    <div class="col-lg-3">
+                                        <input type="text" name="dui" class="form-control form-control-lg form-control-solid" value="{{ $comercio->dui }} " />
+                                    </div>
+                                    <label class="col-lg-2 col-form-label">Tipo de cuenta</label>
+                                    <div class="col-lg-3">
+                                        <select name="tipo_cuenta" id="tipo_cuenta" class="form-control form-control-lg form-control-solid" data-control="select2" data-placeholder="Seleccionar Tipo de cuenta" required>
+                                            <option value="{{ $comercio->tipo_cuenta }}">{{ $comercio->tipo_cuenta }}</option>
+                                            <option value="Ahorros">Ahorros</option>
+                                            <option value="Corriente">Corriente</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="row mb-6">
+                                    <label class="col-lg-3 col-form-label">Cuenta de chivo</label>
+                                    <div class="col-lg-3">
+                                        <input type="text" name="chivo" class="form-control form-control-lg form-control-solid" value="{{ $comercio->chivo }} " />
+                                    </div>
+                                    <label class="col-lg-2 col-form-label">Cuenta tigo money</label>
+                                    <div class="col-lg-3">
+                                        <input type="text" name="tigo" class="form-control form-control-lg form-control-solid" value="{{ $comercio->tigo }} " />
+                                    </div>
+                                </div>
+                                <div class="separator my-10"></div>
+                                <h4 class="text-center">
+                                    Datos de la empresa
+                                </h4>
+                                <br>
+                                <div class="row mb-6">
+                                    <label class="col-lg-3 col-form-label">Nombre de la empresa</label>
+                                    <div class="col-lg-8">
+                                        <input type="text" name="empresa" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" value="{{ $comercio->empresa }} " />
+                                    </div>
+                                </div>
+                                <div class="row mb-6">
+                                    <label class="col-lg-3 col-form-label">Tipo de contribuyente</label>
+                                    <div class="col-lg-3">
+                                        <select name="tipo_contribuyente" id="Tipo_contribuyente" class="form-control form-control-lg form-control-solid" data-control="select2" data-placeholder="Seleccionar Tipo de contribuyente" required>
+                                            <option value="{{ $comercio->tipo_contri }}">{{ $comercio->tipo_contri }}</option>
+                                            <option value="pequeño">Pequeño</option>
+                                            <option value="Mediano">Mediano</option>
+                                            <option value="Grande">Grande</option>
+                                        </select>
+                                    </div>
+                                    <label class="col-lg-2 col-form-label">Giro</label>
+                                    <div class="col-lg-3">
+                                        <input type="text" name="giro" class="form-control form-control-lg form-control-solid" value=">{{ $comercio->giro }} "/>
+                                    </div>
+
+                                </div>
+                                <div class="row mb-6">
+                                    <label class="col-lg-3 col-form-label">Numero de IVA</label>
+                                    <div class="col-lg-3">
+                                        <input type="text" name="nit" class="form-control form-control-lg form-control-solid" value="{{ $comercio->iva }} " />
+                                    </div>
+                                    <label class="col-lg-2 col-form-label">Numero de regristro (NCR)</label>
+                                    <div class="col-lg-3">
+                                        <input type="text" name="ncr" class="form-control form-control-lg form-control-solid" value="{{ $comercio->nrc }} " />
+                                    </div>
+                                </div>
+                                <div class="row mb-6">
+                                    <label class="col-lg-3 col-form-label">Dirección Fisical</label>
+                                    <div class="col-lg-8">
+                                        <input type="text" name="direccionf" class="form-control form-control-lg form-control-solid" value="{{ $comercio->d_fiscal }} " />
+                                    </div>
+                                </div>
+                                <div class="card-footer d-flex justify-content-end py-6 px-9">
+                                    <a href="/comercio" class="btn btn-primary btn-active-light-primary me-2">Cancelar</a>
+                                    <button type="submit" class="btn btn-primary" id="kt_account_profile_details_submit">Editar Cambios</button>
+                                </div>
+                            </div>
+                        </form>
+
+
+
+
+
+
+
+<!--end::Editar-->           
+ </div>
+
+
+
+
+
             <!--end:::Tab pane-->
 
             <!--begin:::Tab pane-->
