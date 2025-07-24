7eccce6d38ed923a49817731467287387000c60a
Robin Castillo <robin.castillo@catolica.edu.sv>
2025-07-24 16:08:56 -0600

    cambios en movimientos jefe

--- resources/views/caja/jefe.blade.php
+++ resources/views/caja/jefe.blade.php
@@ -329,7 +329,7 @@ if (searchText == "") {
                                     <input style="width: 220px;" class="form-control" placeholder="Rango" id="kt_ecommerce_report_shipping_daterangepicker" name="rango" />
   </div>
   </td>
-  <td style="width: 30%;">
+  <td style="width: 30%;"> 
                                     <div class=" ">
 
                                     <select class="form-select form-select-solid mi-selector" data-control="select2" name="usuario" id="usuario" >
@@ -370,9 +370,9 @@ if (searchText == "") {
     <div class="card-body pt-0" style="min-height: 550px;">
         
 <!--begin::Table-->
-<div  id="kt_ecommerce_report_returns_table_wrapper" class="dt-container dt-bootstrap5 dt-empty-footer"><div id="" class="table-responsive">
-    
-<table class="table align-middle table-bordered fs-6 gy-5 dataTable" id="kt_ecommerce_report_shipping_table" style="width: 100%;"><colgroup><col data-dt-column="0" style="width: 124.688px;"><col data-dt-column="1" style="width: 192.797px;"><col data-dt-column="2" style="width: 192.766px;"><col data-dt-column="3" style="width: 190.156px;"><col data-dt-column="4" style="width: 148.828px;"><col data-dt-column="5" style="width: 146.266px;"></colgroup>
+<div  id="kt_ecommerce_report_returns_table_wrapper" class="dt-container dt-bootstrap5 dt-empty-footer"><div id="" class="table-responsive">   
+<table class="table align-middle table-bordered fs-6 gy-5 dataTable" id="kt_ecommerce_report_shipping_table" style="width: 100%;">
+    <colgroup><col data-dt-column="0" ><col data-dt-column="1" ><col data-dt-column="2" ><col data-dt-column="3" ><col data-dt-column="4" ><col data-dt-column="5" ></colgroup>
     <thead>
         <tr class="text-center text-gray-500 fw-bold fs-7 text-uppercase gs-0">
             <th class="text-center min-w-100px dt-orderable-asc dt-orderable-desc" data-dt-column="0" rowspan="1" colspan="1" aria-label="Date: Activate to sort" tabindex="0">
