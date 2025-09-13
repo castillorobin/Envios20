c4b9fd9e4f17233f65f05ecd2fe1eaf5d016ca5f
Robin Castillo <robin.castillo@catolica.edu.sv>
2025-09-13 09:41:37 -0600

    cambios en tamaño

--- resources/views/stocks/asignarrepartidorcaja.blade.php
+++ resources/views/stocks/asignarrepartidorcaja.blade.php
@@ -144,7 +144,7 @@
                             </div>
                             <!--end::Card header-->
                             <!--begin::Card body-->
-                            <div class="card-body pt-0" style="background-color:white; min-height: 610px;  ">
+                            <div class="card-body pt-0" style="background-color:white; min-height: 700px;  ">
 
 
                                 <!--begin::Table-->
